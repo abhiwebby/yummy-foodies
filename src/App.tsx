6099a449ac2ec03c8deadee33eb77d97763e36bf
abhiwebby <abhi.webby@gmail.com>
2021-01-06 08:13:05 +0530

    bootstrap included

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hellow React world!</h1>
+      <h1>Front End</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, ut. Sed
+        ipsam inventore consequuntur praesentium iste placeat temporibus
+        repellat nesciunt ab facilis commodi aut accusantium nemo, labore
+        dolores? Voluptas, nesciunt.
+      </p>
+      <button className="btn btn-danger">submit</button>
+      <button className="btn btn-primary">submit</button>
+      <button className="btn btn-secondary">submit</button>
     </div>
   );
 }
