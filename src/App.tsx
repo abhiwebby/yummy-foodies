a03f3f7fdb55ee370434ba54b851c4806a50fe55
abhiwebby <abhi.webby@gmail.com>
2021-01-07 23:29:25 +0530

    home page edited

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Home from "./Components/Home";
+import Home from "./components/Home";
 import Footer from "./shared/Footer";
 import Header from "./shared/Header";
 
