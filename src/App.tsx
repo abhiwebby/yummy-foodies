90a59eaac1379ce7794232a5ce750c0bee6acf58
abhiwebby <abhi.webby@gmail.com>
2021-01-07 06:29:58 +0530

    Header , footer, shared components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 import "./App.css";
+import Home from "./Components/Home";
+import Footer from "./shared/Footer";
+import Header from "./shared/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Front End</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, ut. Sed
-        ipsam inventore consequuntur praesentium iste placeat temporibus
-        repellat nesciunt ab facilis commodi aut accusantium nemo, labore
-        dolores? Voluptas, nesciunt.
-      </p>
-      <button className="btn btn-danger">submit</button>
-      <button className="btn btn-primary">submit</button>
-      <button className="btn btn-secondary">submit</button>
-      <h1>Hellow React world!</h1>
-      <h4>another branch </h4>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
