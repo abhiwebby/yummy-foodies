72f17b7e06779e026765dd2ee64630017b835b6e
abhiwebby <abhi.webby@gmail.com>
2021-01-09 10:46:55 +0530

    merge box

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Dashboard from "./dashboard/Dashboard";
 import Footer from "./shared/Footer";
 import Header from "./shared/Header";
-import Dataservice from "./services/Dataservice";
 import MainRouter from "./routes/MainRouter";
 import "./App.css";
 import Home from "./components/Home";
